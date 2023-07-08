fd352a085963a19fe727c9398ad6b9e125a4d713
psychoSherlock <pschosherlock@gmail.com>
2023-07-09 03:34:55 +0530

    Top Dock Updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import UpperDock from "./components/UpperDock";
 import "./styles/styles.css";
 import "./styles/docks.css";
 import LowerDock from "./components/LowerDock";
+import FinderWindow from "./components/FinderWindow";
 function App() {
   return (
     <div className="App">
       <UpperDock />
+      <FinderWindow />
       {/* <Dock /> */}
       {/* <h1>Hello</h1> */}
       <LowerDock />
